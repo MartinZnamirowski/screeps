c5f750fd672768ef5ee363680a11e6210eb5b3fe
KaterchenSama <znamirowski.martin@gmail.com>
2018-06-23 10:19:30 +0200

    log

--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ var logger = {
 
     /** @param {string} message **/
     log: function(message, level) {
-        if (Game.memory.globals.logLevel <= level) {
+        if (Memory.globals.logLevel <= level) {
             console.log(message)
         }
     }

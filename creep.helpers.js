c0745978c38f0cc907c7e9c6b361f9ed8105bb4f
KaterchenSama <znamirowski.martin@gmail.com>
2018-06-28 22:13:43 +0200

    fshdjk

--- creep.helpers.js
+++ creep.helpers.js
@@ -3,6 +3,7 @@ var logger = require('logger');
 var creepHelpers = {
     getHarvestingPower: function(creep) {
         if(creep.memory.role=='harvester') {
+            logger.log("LALA", 5)
             var workModules = 0
             for (var i = 0, len = creep.body.length; i < len; i++) {
                 const creepModule = creep.body[i]

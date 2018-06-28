85421a2c4675301546e89aaa87f1db68fa7d97d5
KaterchenSama <znamirowski.martin@gmail.com>
2018-06-28 23:59:18 +0200

    dfjskld

--- role.harvester.js
+++ role.harvester.js
@@ -1,18 +1,51 @@
-var roleHarvester = {
+var harvesterHelper = {
     determineHarvestingSlot: function(creep) {
         var sources = creep.room.find(FIND_SOURCES);
 
+        harvestMapping = {}
+
+        const myHarvesters = room.find(FIND_MY_CREEPS, {
+            filter: (creep) => {
+                return creep.memory.role=='harvester'
+            }
+        })
+        for (var i = 0, len = myHarvesters.length; i < len; i++) {
+            const myHarvester = myHarvesters[i]
+            if(myHarvester.memory.target == undefined) {
+                continue
+            } 
+            if(!myHarvester.memory.target in harvestMapping) {
+                harvestMapping[myHarvester.memory.target] = 1
+            } else {
+                harvestMapping[myHarvester.memory.target] += 1
+            }
+        }
+
+        // TODO ORDER SOURCES BY DISTANCE
+        for (sourceID in creep.room.memory.harvestSlots) {
+            if(harvestMapping[sourceID] < creep.room.memory.harvestSlots[sourceID]) {
+                return sourceID
+            }
+        }            
     },
+}
+
+var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+        if(creep.carry.energy < creep.carryCapacity && creep.memory.target == undefined) {
+            var target = harvesterHelper.determineHarvestingSlot(creep)
+            creep.memory.target = target
+        } else if (creep.carry.energy == creep.carryCapacity){
+            creep.memory.target = undefined
         }
-        else {
+
+        if(!creep.memory.target == undefined) {
+            if(creep.harvest(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        } else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
